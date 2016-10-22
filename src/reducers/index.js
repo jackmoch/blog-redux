c30cbf34c453728b70e77dc64595b9ead76f0bc7
Jack Mocherman <mochermanjack@gmail.com>
2016-10-22 16:36:08 -0500

    fixed syntax error, missing comma

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { reducer as formReducer } from 'redux-form'
 import PostsReducer from './reducer_posts'
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
   form: formReducer
 });
 
