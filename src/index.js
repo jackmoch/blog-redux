a527a60310e9db39882ef24b768828395ba99bdf
Jack Mocherman <mochermanjack@gmail.com>
2016-10-22 15:26:10 -0500

    added Router component to main js file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { Router, browserHistory } from 'react-router'
 import createLogger from 'redux-logger'
+
 import reducers from './reducers'
+import routes from './routes'
 
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)} >
-		<Router history={browserHistory} />
+		<Router history={browserHistory} routes={routes} />
 	</Provider>,
 	document.querySelector('.container')
 )
